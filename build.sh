f5fd3edb20a00b27898189d9ceff8c762e3654b4
zunichky <zunichky@msu.edu>
2023-04-12 16:12:02 -0400

    build script

---
+++ build.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+/home/dev/Unity/Hub/Editor/2020.3.11f1/Editor/Unity -quit -batchmode -projectPath /home/dev/code/csi6160_reinforcement/ -buildLinux64Player builds/linux_AI/build.X86_64
